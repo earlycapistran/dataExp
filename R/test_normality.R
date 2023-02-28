4c3de7f67365d10d1095c07dc24bd284034ac591
Michelle María Early Capistrán <47227446+earlycapistran@users.noreply.github.com>
2023-02-28 15:11:15 -0800

    Added roxygen documentation

--- R/test_normality.R
+++ R/test_normality.R
@@ -1,4 +1,19 @@
-test_normality <- function(data, variable) {
+# =============================================================================
+# Test Normality
+# earlyc@stanford.edu, February 2023
+# =============================================================================
+
+#' Performs Shapiro-Wilk test and makes normality plots (qq)
+#' 
+#' @param data a dataframe
+#' @param variable variable name passed as a string
+#' @return normality plot and Shapiro-Wilke test results
+#' @export
+#' @example 
+#' test_normality(iris, "Sepal.Length")
+#' 
+
+tests_norm <- function(data, variable) {
   par(mfrow = c(1, 1))
   qqnorm(data[[variable]])
   qqline(data[[variable]])
@@ -6,4 +21,4 @@ test_normality <- function(data, variable) {
   print(norm_test)
 }
 
-test_normality(penguins, "body_mass_g")
+tests_norm(penguins, body_mass_g)

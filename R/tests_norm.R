8f5e5b89eb09824abfee4c5dac44d91df3c8df23
Michelle María Early Capistrán <47227446+earlycapistran@users.noreply.github.com>
2023-02-28 15:34:32 -0800

    Added functions to plot histograms with normality and density curves

--- R/tests_norm.R
+++ R/tests_norm.R
@@ -14,11 +14,36 @@
 #' 
 
 tests_norm <- function(data, variable) {
-  par(mfrow = c(1, 1))
+  
+  # Make normality plot ---
+  par(mfrow = c(2, 2)) # set up grid
   qqnorm(data[[variable]])
-  qqline(data[[variable]])
+  qqline(data[[variable]], col = "red")
+  
+  # Make histogram with normality curve ---
+  data <- na.omit(data) # Remove missing values
+  myhist <- hist(data[[variable]], main = "Histogram") # Make histogram
+  # Define multiplier to convert density to counts
+  multiplier <- myhist$counts / myhist$density 
+  mydensity <- density(data[[variable]]) 
+  mydensity$y <- mydensity$y * multiplier[1] 
+  
+  plot(myhist, main = "Histogram with density curve")
+  lines(mydensity, col = 4)
+  
+  # Normal curve 
+  myX <- seq(min(data[[variable]]), max(data[[variable]]), length.out= 100)
+  mymean <- mean(data[[variable]])
+  mysd <- sd(data[[variable]])
+  
+  normal <- dnorm(x = myX, mean = mymean, sd = mysd)
+  
+  plot(myhist, main = "Histogram with normal curve")
+  lines(myX, normal * multiplier[1], col = "red", lwd = 1)
+  
+  # Run normality tests ----
   norm_test <- shapiro.test(data[[variable]])
   print(norm_test)
 }
 
-tests_norm(penguins, body_mass_g)
+tests_norm(penguins, "body_mass_g")

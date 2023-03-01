f2e160d8bd17b9d75633c641a9a0578cd55061de
Michelle María Early Capistrán <47227446+earlycapistran@users.noreply.github.com>
2023-03-01 12:18:28 -0800

    Added Levene test

--- R/test_variance.R
+++ R/test_variance.R
@@ -1,7 +1,10 @@
   tests_variance <- function(data, data_var, group_var) {
-    # Load library
+  # Load library
+  library("car")
+    
+    # Run tests ---
   
-   # Run Bartlett test
+    # Run Bartlett test
     btest <- bartlett.test(data[[data_var]], data[[group_var]])
     # Print with string for variable names
     cat(paste("Bartlett test for homogeneity of variance", "\n",
@@ -9,8 +12,14 @@
               "Bartlett's K-squared:", btest$statistic, "\n",
               "df: ", btest$parameter, "\n",
               "p-value:", btest$p.value))
-    print(btest)
-
+    
+    # Run Levene Test
+    myLevene <- car::leveneTest(y = data[[data_var]], 
+                                group = data[[group_var]])
+    
+    # Insert spaces between test print-outs for legibility
+    cat("\n", "\n", "---------------------" , "\n", "\n")
+    print(myLevene)
   }
   
   tests_variance(iris, "Sepal.Length", "Species")

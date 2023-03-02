ac5bb89c33e466c200d29b895694350cccedba75
Michelle María Early Capistrán <47227446+earlycapistran@users.noreply.github.com>
2023-03-01 16:13:29 -0800

    Added deparsing so variable names aren't strings

--- R/test_variance.R
+++ R/test_variance.R
@@ -4,52 +4,41 @@ tests_variance <- function(df, data_var, group_var) {
   library("ggplot2")
   library("gridExtra")
 
-    # Run tests ---
+  df <- na.omit(df) # Remove missing values
   
-    # Run Bartlett test
-    btest <- bartlett.test(df[[data_var]], df[[group_var]])
-    # Print with string for variable names
-    cat(paste("Bartlett test for homogeneity of variance", "\n",
-              "Data:", data_var, "and", group_var, "\n",
-              "Bartlett's K-squared:", btest$statistic, "\n",
-              "df: ", btest$parameter, "\n",
-              "p-value:", btest$p.value))
-    
+  # Deparse variable names
+  data_var <- deparse(substitute(data_var))
+  group_var <- deparse(substitute(group_var))
+  print(data.class(data_var))
+  
     # Run Levene Test
-    myLevene <- car::leveneTest(y = df[[data_var]], 
+    myLevene <- car::leveneTest(y = df[[data_var]],
                                 group = df[[group_var]])
-    
+
     # Insert spaces between test print-outs for legibility
     cat("\n", "\n", "---------------------" , "\n", "\n")
     print(myLevene)
 
     # Make simple plots ---
     par(mfrow = c(2, 2)) # set up grid
-    
-    # Boxplot with base R
-    # boxplot(data[[data_var]] ~ data[[group_var]], 
-    #         data = data,
-    #         xlab = paste(group_var),
-    #         ylab = paste(data_var),
-    #         col = factor(group))
-    
+
     # Boxplot with ggplot
-    boxPlot <-ggplot(data = df, aes(x = .data[[group_var]], 
-                        y = .data[[data_var]], 
+    boxPlot <-ggplot(data = df, aes(x = .data[[group_var]],
+                        y = .data[[data_var]],
                         fill = .data[[group_var]])) +
       geom_boxplot(outlier.size=2) +
       theme_classic()
-    
+
     # Make a boxplot with dots
-    boxDots <- boxPlot + 
-      geom_dotplot(binaxis='y', 
-                   stackdir='center', 
+    boxDots <- boxPlot +
+      geom_dotplot(binaxis='y',
+                   stackdir='center',
                    dotsize=1,
-                   fill = "grey", 
+                   fill = "grey",
                    alpha = 0.5)
-    
+
     # Density plot by group
-    densPlot <- ggplot(data = df, aes(x = .data[[data_var]], 
+    densPlot <- ggplot(data = df, aes(x = .data[[data_var]],
                    fill = .data[[group_var]],
                    )) +
       geom_density(alpha = 0.5)
@@ -57,16 +46,3 @@ tests_variance <- function(df, data_var, group_var) {
     # Arrange the plots on a grid
     gridExtra::grid.arrange(boxPlot, boxDots, densPlot, nrow = 2)
   }
-  
-tests_variance(iris, "Sepal.Length", "Species")
-
-# p<-ggplot(iris, aes(x=Species, y=Sepal.Length, fill=Species)) +
-#   geom_boxplot() +
-#   theme_classic()
-# p
-
-# densPlot <- ggplot(data = iris, aes(x = Sepal.Length,
-#                                   fill = Species)) +
-#   geom_density()
-# 
-# densPlot

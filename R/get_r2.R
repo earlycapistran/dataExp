575b2ab439284765a4aa473d98965e7e172561ae
Michelle María Early Capistrán <47227446+earlycapistran@users.noreply.github.com>
2023-03-23 11:25:31 -0700

    Converted to tidyverse style

--- R/get_r2.R
+++ R/get_r2.R
@@ -2,21 +2,15 @@
 # Get R2
 # earlyc@stanford.edu, March 2023
 # =============================================================================
-
-getR2 <- function(model_object) {
-  
+get_r2 <- function(model_object) {
   # Check object type
   model_type <- class(model_object)[1]
-  r_sq <- switch(
-    model_type,
+  r_sq <- switch(model_type,
     lm = 1 - stats::var(stats::residuals(model_object)) /
       stats::var(model_object$model[[1]]),
     glm = 1 - model_object$deviance / model_object$null.deviance
   )
-  # Calculate R2 
-  # r_sq <-1 - (model_object$deviance/model_object$null.deviance)
-  # r_sq <- round(r_sq, 3)
-  
+  return(r_sq)
   # Print
   cat("R²:", "\n", paste(r_sq))
 }
